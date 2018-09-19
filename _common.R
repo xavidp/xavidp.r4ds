daf522af6f443ad847f7f101d3ec34a40203753f
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-09-19 16:36:56 -0300

    corrige PR #24

--- _common.R
+++ _common.R
@@ -6,9 +6,9 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   cache = TRUE,
   out.width = "70%",
-  fig.align = 'center',
+  fig.align = "center",
   fig.width = 6,
-  fig.asp = 0.618,  # 1 / phi
+  fig.asp = 0.618, # 1 / phi
   fig.show = "hold"
 )
 
