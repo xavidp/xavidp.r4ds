1d385f2715794d0755bf2306b143a58876ae09a7
Pachamaltese <mauriciovargas@ug.uchile.cl>
2019-02-19 18:31:57 -0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Rscript -e 'devtools::install_github("hadley/r4ds", force = TRUE)'
-Rscript -e 'devtools::install_github("cienciadedatos/datos", force = TRUE)'
+Rscript -e 'devtools::install_github("hadley/r4ds")'
+Rscript -e 'try(remove.packages("datos")); devtools::install_github("pachamaltese/datos")'
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
