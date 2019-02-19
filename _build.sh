d5d26a9ac653fb5fbc4c6bd782492b487c26502b
pachamaltese <mvargas@dcc.uchile.cl>
2019-02-19 19:40:23 -0300

    remove cache from chunks

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 Rscript -e 'devtools::install_github("hadley/r4ds")'
-Rscript -e 'try(remove.packages("datos")); devtools::install_github("pachamaltese/datos")'
+Rscript -e 'devtools::install_github("pachamaltese/datos")'
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
