3b8329a8f36db4e3b84d4ac5c207839427d1cf9d
pachamaltese <mvargas@dcc.uchile.cl>
2019-02-20 01:01:12 +0000

    weekly build 2019-02-20 01:01:12

--- _build.sh
+++ _build.sh
@@ -7,6 +7,7 @@ git checkout traduccion
 R --vanilla << EOF
 
 if (!require("pacman")) { install.packages("pacman") }
+
 pacman::p_load_current_gh(c("pachamaltese/r4ds"))
 
 bookdown::render_book('index.Rmd', 'bookdown::gitbook')
@@ -14,8 +15,8 @@ bookdown::render_book('index.Rmd', 'bookdown::gitbook')
 q()
 EOF
 
-#git push origin --delete gh-pages
+git push origin --delete gh-pages
 
-#git add . && git commit -m "weekly build `date +'%Y-%m-%d %H:%M:%S'`"
+git add . && git commit -m "weekly build `date +'%Y-%m-%d %H:%M:%S'`"
 
-#git subtree push --prefix docs origin gh-pages
+git subtree push --prefix docs origin gh-pages
