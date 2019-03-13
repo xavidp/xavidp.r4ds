949a10d97ef46bbeecd0af800904d1d9d57e6033
pachamaltese <mvargas@dcc.uchile.cl>
2019-03-13 13:11:16 -0300

    remove example.R

--- 00-example.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(modelr)
-
-diamonds2 <- diamonds %>%
-  filter(carat <= 2.5) %>%
-  mutate(lprice = log2(price), lcarat = log2(carat))
-
-ggplot(diamonds2, aes(lcarat, lprice)) +
-  geom_hex(bins = 50)
